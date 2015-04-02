e2993f9e36b170c681adcbe1f1dc671f8fcc98fc
Daniil Molodkov <mr.me@daniil.ca>
2015-04-02 16:50:01 -0400

    Added race track matrix update test with obstacles

--- scripts/race-track.js
+++ scripts/race-track.js
@@ -18,4 +18,47 @@ class RaceTrack {
 
     document.querySelectorAll('#race-track')[0].innerHTML = raceTrackEl;
   }
+
+  update(matrix) {
+    var self = this;
+
+    matrix.forEach(function(trackRow, i) {
+      trackRow.forEach(function(trackCol, j) {
+        let currTrackCol = document.querySelectorAll('.track-row-' + (i + 1) + ' > .track-col-' + (j + 1))[0];
+
+        self._resetTrackCol(currTrackCol);
+
+        self._updateTrackCol(currTrackCol, trackCol);
+      });
+    });
+  }
+
+  _resetTrackCol(col) {
+    ['icon-shield', 'icon-cool', 'icon-evil']
+    .forEach(function(className) {
+      if (col.classList)
+        col.classList.remove(className);
+      else
+        col.className = col.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    });
+  }
+
+  _updateTrackCol(col, val) {
+    let className = '';
+
+    switch (val) {
+      case 'x':
+        className = 'icon-shield';
+        break;
+    }
+
+    if (className.trim() === '') {
+      return;
+    }
+
+    if (col.classList)
+      col.classList.add(className);
+    else
+      col.className += ' ' + className;
+  }
 }
